5a3e2189dc053f1edfe5e44d9ee5d1359fe934a2
Marco Canton <67104790+MarcoCanton1@users.noreply.github.com>
2022-05-16 15:01:50 -0300

    yipee

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemigo : MonoBehaviour
+{
+    public Transform jugador;
+    public NavMeshAgent agente;
+    
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agente.SetDestination(jugador.position);
+    }
+}
