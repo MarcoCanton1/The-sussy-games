664711dac4b19f247fb2ef4c6fa2644e90863869
Marco Canton <67104790+MarcoCanton1@users.noreply.github.com>
2022-05-16 18:37:24 -0300

    final
    si

--- Assets/Scripts/Moverse.cs
+++ Assets/Scripts/Moverse.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Moverse : MonoBehaviour
 {
@@ -12,6 +14,12 @@ public class Moverse : MonoBehaviour
     public LayerMask piso;
     bool sentao;
     public float arrastrasuelo;
+    public Image susto;
+    public AudioSource audio;
+    public AudioSource atacama;
+    public AudioClip cuidado;
+    public InputField code;
+    bool ending = false;
 
     Vector3 DireccionMov;
 
@@ -20,6 +28,9 @@ public class Moverse : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        susto.gameObject.SetActive(false);
+        audio.gameObject.GetComponent<AudioSource>();
+        code.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -35,6 +46,14 @@ public class Moverse : MonoBehaviour
         else
         {
             rb.drag = 0;
+        }
+        if (code.text == "46982" && ending == false)
+        {
+            SceneManager.LoadScene("GoodEnding");
+        }
+        else if (code.text == "46982" && ending == true)
+        {
+            SceneManager.LoadScene("TrueEnding");
         }
     }
 
@@ -63,5 +82,41 @@ public class Moverse : MonoBehaviour
             Vector3 limite = flatvel.normalized * velMov;
             rb.velocity = new Vector3(limite.x, rb.velocity.y, limite.z);
         }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "atacama")
+        {
+            StartCoroutine(screamer());
+        }
+
+        if (col.gameObject.name == "salvacion")
+        {
+            atacama.gameObject.SetActive(false);
+            code.gameObject.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else if (col.gameObject.name == "salvacion" && ending == true)
+        {
+            atacama.gameObject.SetActive(false);
+            code.gameObject.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (col.gameObject.name == "sussy")
+        {
+            Debug.Log("si");
+            ending = true;
+        }
+    }
+
+    IEnumerator screamer()
+    {
+        atacama.mute = true;
+        susto.gameObject.SetActive(true);
+        audio.PlayOneShot(cuidado, 0.5f);
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("BadEnding");
     }
 }
